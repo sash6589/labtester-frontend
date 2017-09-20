ef680ac7f0e66ce75fce508fe1a5959e42cf31b8
sash6589 <sash6589@yandex-team.ru>
2017-09-20 18:10:49 +0300

    more output in main run script

--- run_labtester.sh
+++ run_labtester.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-./run_labtester-frontend.sh
+echo "Running labtester-worker"
 ../labtester-worker/run_labtester-worker.sh
+
+echo "Running labtester-master"
 ../labtester-master/run_labtester-master.sh
+
+echo "Running labtester-frontend"
+./run_labtester-frontend.sh
