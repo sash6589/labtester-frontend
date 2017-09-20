4a2b388fdf5b2ea1c7782dd9bd886712b06cd994
sash6589 <sash6589@yandex-team.ru>
2017-09-20 18:24:22 +0300

    reorder components for starting

--- run_labtester.sh
+++ run_labtester.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Running labtester-worker"
-cd ../labtester-worker
-./run_labtester-worker.sh
-
 echo "Running labtester-master"
 cd ../labtester-master
 ./run_labtester-master.sh
+
+echo "Running labtester-worker"
+cd ../labtester-worker
+./run_labtester-worker.sh
 
 echo "Running labtester-frontend"
 cd ../labtester-frontend
