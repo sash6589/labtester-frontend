4f9f35c54ba1ba0d30980f48592bb923574aa545
sash6589 <sash6589@yandex-team.ru>
2017-09-27 17:38:11 +0300

    add install_labtester script

---
+++ install_labtester.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Installing postgres"
+sudo apt-get update
+sudo apt-get install postgresql postgresql-contrib
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
+echo "host all all all md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+invoke-rc.d postgresql reload
+sudo service postgresql restart
