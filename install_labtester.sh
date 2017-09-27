97f4afce24700d96fff1f63899c18e2f54258aa9
sash6589 <sash6589@yandex-team.ru>
2017-09-27 17:43:54 +0300

    add comment

--- install_labtester.sh
+++ install_labtester.sh
@@ -7,3 +7,4 @@ sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgre
 echo "host all all all md5" >> /etc/postgresql/9.5/main/pg_hba.conf
 invoke-rc.d postgresql reload
 sudo service postgresql restart
+echo "Postgres has installed successful. You need change password for postgres user in postgres."
