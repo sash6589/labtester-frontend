43c1ee8b96a3558b94d6098f02f7887606b5d3aa
sash6589 <sash6589@yandex-team.ru>
2017-10-04 19:36:53 +0300

    change password in db

--- install_labtester.sh
+++ install_labtester.sh
@@ -20,8 +20,6 @@ echo "Installing postgres"
 sudo apt-get install postgresql postgresql-contrib
 sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
 echo "host all all all md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+sudo -u postgres psql postgres -c "ALTER USER postgres WITH PASSWORD 'labteste';"
 sudo invoke-rc.d postgresql reload
 sudo service postgresql restart
-echo "Postgres has installed successful. You need change password for postgres user in postgres."
-
-echo "Installing java"
