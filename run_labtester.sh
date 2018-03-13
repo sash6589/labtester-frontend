d50497e43156e4e03938cf267d237fd33500e842
Alexander Matveev <am@veeroute.com>
2018-03-13 19:48:59 +0300

    pass parameter to worker script

--- run_labtester.sh
+++ run_labtester.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+  then
+    echo "run ./run_labtester-worker.sh DOTOKEN"
+    exit
+fi
+
 echo "Running labtester-master"
 cd ../labtester-master
 ./run_labtester-master.sh
 
 echo "Running labtester-worker"
 cd ../labtester-worker
-./run_labtester-worker.sh
+./run_labtester-worker.sh $1
 
 echo "Running labtester-frontend"
 cd ../labtester-frontend
